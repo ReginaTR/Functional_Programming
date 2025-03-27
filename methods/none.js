5506da4cc9bc5a91186d07d742735d8eb64f2171
ReginaTR <regininhar@hotmail.com>
2025-03-27 18:23:19 -0300

    none

--- methods/none.js
+++ methods/none.js
@@ -16,3 +16,7 @@ console.log(none([1, 2, 3, 4, 5], (item => item < 10)))
 console.log(none([1, 2, 3, 4, 5], (item => item > 10)))
 
 console.log(none([1, 2, 3, 4, 5], (item => item % 2 !== 0)))
+
+//const none = (item, i) => !some(item, i)
+//const none = (item, i) => every(item, i)
+//const none = (collection, callbackFn) => every(collection, (item, i) => !callbackFn(item, i))
